e5f0c8e197b5dd7f9d54b6e0322d9acb9cc15dce
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-05-22 22:16:26 +0600

    banner added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+          <img
+            src="https://i.ibb.co/5Y0HGjz/banner1.jpg"
+            alt=""
+            class="w-full"
+          />
+        </div>
+        <div id="item2" class="carousel-item w-full">
+          <img
+            src="https://i.ibb.co/yddCTZr/banner2.jpg"
+            alt=""
+            class="w-full"
+          />
+        </div>
+        <div id="item3" class="carousel-item w-full">
+          <img
+            src="https://i.ibb.co/Mkvb60M/banner3.jpg"
+            alt=""
+            class="w-full"
+          />
+        </div>
+        <div id="item4" class="carousel-item w-full">
+          <img
+            src="https://i.ibb.co/bvjGm3X/banner4.jpg"
+            alt=""
+            class="w-full"
+          />
+        </div>
+      </div>
+      <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-sm btn-primary ">
+          1
+        </a>
+        <a href="#item2" class="btn btn-sm btn-primary ">
+          2
+        </a>
+        <a href="#item3" class="btn btn-sm btn-primary font-semibold">
+          3
+        </a>
+        <a href="#item4" class="btn btn-sm btn-primary ">
+          4
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
