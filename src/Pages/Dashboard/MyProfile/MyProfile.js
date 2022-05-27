11f70c28f67533a48b68a494050d4cbe1d4c4096
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-05-27 22:52:52 +0600

    profile  added

--- src/Pages/Dashboard/MyProfile/MyProfile.js
+++ src/Pages/Dashboard/MyProfile/MyProfile.js
@@ -16,8 +16,8 @@ const MyProfile = () => {
   const { data: profile, refetch } = useReactQuery(url, header);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-10">
-      <ViewProfile key={profile._id} profile={profile} />
-      <UpdateProfile key={profile._id} refetch={refetch} />
+      <ViewProfile key={profile?._id} profile={profile} />
+      <UpdateProfile refetch={refetch} />
     </div>
   );
 };
